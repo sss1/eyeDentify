5c3e9b75dae9d92e6849b8dafff1da262f8115c0
sss1 <sss1@andrew.cmu.edu>
2019-09-22 14:55:18 -0400

    Added first draft of actual experiment data_collector and object_smoothing, with centroid_tracking and multiple stimulus videos.

--- data_collector.py
+++ data_collector.py
@@ -1,27 +1,257 @@
-import numpy as np
-import cv2
+# -----------------------------------------------------------------------
+#
+# (c) Copyright 1997-2013, SensoMotoric Instruments GmbH
+# 
+# Permission  is  hereby granted,  free  of  charge,  to any  person  or
+# organization  obtaining  a  copy  of  the  software  and  accompanying
+# documentation  covered  by  this  license  (the  "Software")  to  use,
+# reproduce,  display, distribute, execute,  and transmit  the Software,
+# and  to  prepare derivative  works  of  the  Software, and  to  permit
+# third-parties to whom the Software  is furnished to do so, all subject
+# to the following:
+# 
+# The  copyright notices  in  the Software  and  this entire  statement,
+# including the above license  grant, this restriction and the following
+# disclaimer, must be  included in all copies of  the Software, in whole
+# or  in part, and  all derivative  works of  the Software,  unless such
+# copies   or   derivative   works   are   solely   in   the   form   of
+# machine-executable  object   code  generated  by   a  source  language
+# processor.
+# 
+# THE  SOFTWARE IS  PROVIDED  "AS  IS", WITHOUT  WARRANTY  OF ANY  KIND,
+# EXPRESS OR  IMPLIED, INCLUDING  BUT NOT LIMITED  TO THE  WARRANTIES OF
+# MERCHANTABILITY,   FITNESS  FOR  A   PARTICULAR  PURPOSE,   TITLE  AND
+# NON-INFRINGEMENT. IN  NO EVENT SHALL  THE COPYRIGHT HOLDERS  OR ANYONE
+# DISTRIBUTING  THE  SOFTWARE  BE   LIABLE  FOR  ANY  DAMAGES  OR  OTHER
+# LIABILITY, WHETHER  IN CONTRACT, TORT OR OTHERWISE,  ARISING FROM, OUT
+# OF OR IN CONNECTION WITH THE  SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# -----------------------------------------------------------------------
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import csv
+from datetime import datetime
+from iViewXAPI import  *            #iViewX library
+from iViewXAPIReturnCodes import *
+import threading
 import time
+import cv2 # Use OpenCV to display video
+import screeninfo
+import pickle
 
-video = cv2.VideoCapture('traffic-mini.mp4')
+# ---------------------------------------------
+#---- connect to iViewX
+# ---------------------------------------------
 
-current_frame = 0
-video_start_time = time.time()
+# Ask user for participant number/id
+participant_id = raw_input('Enter participant ID: ')
+today = `datetime.now().month` + "_" + `datetime.now().day` + "_" + `datetime.now().year`
+time_now = `datetime.now().hour` + "_" + `datetime.now().minute`
+eyetracking_file_name = participant_id + "_eyetracking_" + today + "_" + time_now + '.csv'
+print("Eye-tracking data will save to the file: " + eyetracking_file_name)
+eyetracking_writer = csv.writer(open(eyetracking_file_name,'wb'), delimiter = ',')
+header = ["ComputerClock_Timestamp", "Avg_GazeX", "Avg_GazeY", "LeftEye_GazeX", "LeftEye_GazeY", "RightEye_GazeX", "RightEye_GazeY", "LeftEye_Diam", "RightEye_Diam"]
+eyetracking_writer.writerow(header)
 
-FPS = video.get(cv2.CAP_PROP_FPS)
-delay = 1.0/FPS
+print("Preparing eye-tracker...")
 
-while(video.isOpened()):
-    ret, frame = video.read() # Load next frame
-    if not ret: # If done playing video
-      break
+res = iViewXAPI.iV_SetLogger(c_int(1), c_char_p("iViewXSDK_Python_SimpleExperiment.txt"))
+res = iViewXAPI.iV_Connect(c_char_p('127.0.0.1'), c_int(4444), c_char_p('127.0.0.1'), c_int(5555))
+if res != 1:
+    HandleError(res)
+    exit(0)
 
-    cv2.imshow('frame',frame) # Display frame
+res = iViewXAPI.iV_GetSystemInfo(byref(systemData))
 
-    current_frame += 1
-    while time.time() < video_start_time + current_frame * delay:
-      cv2.waitKey(1)
+# ---------------------------------------------
+#---- define callback function that records eye-tracking data and displays video
+# ---------------------------------------------
 
-print('Runtime: ' + str(time.time() - video_start_time))
+# Realign eye-tracking with origin of video.
+def realign(x, y):
+  return (x - xPos, y - yPos)
 
-video.release()
-cv2.destroyAllWindows()
+def SampleCallback(sample):
+
+  # Eye-tracking timestamp (relative to video onset) in milliseconds
+  timestamp = time.time()
+
+  # Record eye-tracking data
+  leftX = sample.leftEye.gazeX
+  leftY = sample.leftEye.gazeY
+  rightX = sample.rightEye.gazeX
+  rightY = sample.rightEye.gazeY
+  leftDiam = sample.leftEye.diam
+  rightDiam = sample.rightEye.diam
+
+  # Recode missing data from 0.0 to NaN.
+  # Take average of left and right eyes, unless one or both eyes is missing.
+  if leftX > 0.0 and rightX > 0.0: # Both eyes captured
+    leftX, leftY = realign(leftX, leftY)
+    rightX, rightY = realign(rightX, rightY)
+    bestX = (leftX + rightX) / 2.0
+    bestY = (leftY + rightY) / 2.0
+
+  elif leftX > 0.0: # Right eye is missing; use left
+    leftX, leftY = realign(leftX, leftY)
+    rightX = rightY = float('nan')
+    bestX = leftX
+    bestY = leftY
+
+  elif rightX > 0.0: # Left eye is missing; use right
+    leftX = leftY = float('nan')
+    rightX, rightY = realign(rightX, rightY)
+    bestX = rightX
+    bestY = rightY
+
+  else: # Both eyes missing; report NaN
+    bestX = bestY = leftX = leftY = rightX = rightY = float('nan')
+
+  # Sometimes, gaze data is missing but pupil diameter is not (or vice versa);
+  # hence, recode missing pupil diameter data as NaN separately
+  if leftDiam == 0.0:
+    leftDiam = float('nan')
+  if rightDiam == 0.0:
+    rightDiam = float('nan')
+
+  # Write timepoint data as new row in output file
+  eyetracking_writer.writerow([str(x) for x in [timestamp, bestX, bestY, leftX, leftY, rightX, rightY, leftDiam, rightDiam]])
+
+  return 0
+
+def EventCallback(event):
+  return 0
+
+# ---------------------------------------------
+#---- implement background thread to run above CallBack
+# ---------------------------------------------
+
+CMPFUNC = WINFUNCTYPE(c_int, CSample)
+smp_func = CMPFUNC(SampleCallback)
+sampleCB = False
+
+CMPFUNC = WINFUNCTYPE(c_int, CEvent)
+event_func = CMPFUNC(EventCallback)
+eventCB = False
+
+class StoppableThread(threading.Thread):
+
+  def __init__(self):
+    threading.Thread.__init__(self)
+
+  def run(self):
+    self.run = True
+    while self.run:
+      res = iViewXAPI.iV_SetSampleCallback(smp_func)
+      sampleCB = True
+      res = iViewXAPI.iV_SetEventCallback(event_func)
+      eventCB = True
+
+  def stop(self):
+    self.run = False
+
+# ---------------------------------------------
+#---- UI to start/stop DataStreaming
+# ---------------------------------------------
+
+command = None
+while (command != 'start'):
+  command = raw_input("Type 'start' and press Enter to begin experiment: ")
+
+
+
+
+
+stimulus_record_file_name = participant_id + "_stimulus_" + today + "_" + time_now + '.csv'
+videos_list = np.random.permutation(range(14)) + 1
+print("Stimulus data data will save to the file: " + stimulus_record_file_name)
+
+with open(stimulus_record_file_name, 'w', newline='') as csvfile:
+  stimulus_writer = csv.writer(data, delimiter = ',')
+  header = ["ComputerClock_Timestamp", "Video", "Frame", "Target_Name", "startX", "startY", "endX", "endY"]
+  stimulus_writer.writerow(header)
+  for video_ID in video_list:
+    video_file_name = 'data/MOT17_videos/' + str(video_ID).zfill(2) + '.mp4'
+    object_file_name = 'data/detected_objects/' + str(video_ID).zfill(2) + '.pickle'
+
+    # ---------------------------------------------
+    # ---- set up video display
+    # ---------------------------------------------
+
+    video = cv2.VideoCapture(video_file_name)
+    with open(object_file_name, 'rb') as in_file:
+      all_frames = pickle.load(in_file)
+    
+    # Get basic video information
+    video_width = video.get(cv2.CAP_PROP_FRAME_WIDTH)
+    video_height = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
+    FPS = video.get(cv2.CAP_PROP_FPS) # natural frame rate
+    
+    # Set the inter-frame delay based on the video's natural framerate
+    delay = 1.0/FPS
+    
+    # Create video display window and center it on the screen
+    screen = screeninfo.get_monitors()[0]
+    # (xPos, yPos) is the upper-left corner of the video display
+    xPos = int((screen.width - video_width) / 2.0)
+    yPos = int((screen.height - video_height) / 2.0)
+    if xPos < 0 or yPos < 0:
+      print('Warning: video size ' + str((video_width, video_height)) + \
+            ' is larger than screen size ' + str((screen.width, screen.height)) + \
+            '.')
+      print('Some of the video may be cut off.')
+    cv2.namedWindow('Video Frame')
+    cv2.moveWindow('Video Frame', xPos, yPos)
+    
+    # Set up to display the first frame
+    current_frame = 0
+    videoStartTime = time.time()
+    nextFrameExists, frame = video.read() # Load first video frame
+    
+    print("Initiating eye-tracking...")
+    # start recording in a background thread
+    thr = StoppableThread()
+    thr.start()
+
+    print("Playing video...")
+    while nextFrameExists: # While there are more frames to display, continue displaying video
+      if time.time() > videoStartTime + current_frame * delay:
+          cv2.imshow('Video Frame', frame) # Display frame
+          cv2.waitKey(1)
+          nextFrameExists, frame = video.read() # Load next video frame
+          target = target_list[current_frame]
+          b = target['box_points']
+          cv2.ellipse(frame, centroid, ((b[2] - b[0])//2, (b[3] - b[1])//2), 0, 0, 360, color = (0, 255, 0), thickness = 2)
+          stimulus_writer.writerow([str(x) for x in [time.time(), video_ID, current_frame, target['name'], b[0], b[1], b[2], b[3]]])
+          current_frame += 1
+
+    video.release()
+    cv2.destroyAllWindows()
+
+    print("Terminating eye-tracking collection...")
+    # tell background thread to stop and wait for it to terminate
+    thr.stop()
+    thr.join()
+
+
+# wait for user to terminate recording
+# command = 0
+# while (command != 'quit'):
+#   command = raw_input("Type 'quit' and press Enter to terminate eye-tracking recording: ")
+
+# ---------------------------------------------
+#---- stop recording and disconnect from iViewX
+# ---------------------------------------------
+
+print("Disconnecting from eye-tracker...")
+
+res = iViewXAPI.iV_Disconnect()
+
+print("Saving eye-tracking data...")
+
+data.flush()
+data.close()
+
+print("Done!")
--- object_smoothing.py
+++ object_smoothing.py
@@ -1,22 +1,142 @@
 import pickle
+import sys, time
+from copy import deepcopy
+import cv2 # Use OpenCV to display video
+import screeninfo
+import numpy as np
 
-def calc_centroid(obj):
-  # Recall that, in Python 3, division is float by default
-  return ((obj['box_points'][0] + obj['box_points'][2])/2,
-          (obj['box_points'][1] + obj['box_points'][3])/2)
+from centroidtracker import CentroidTracker, calc_centroid
 
-def smooth_objects(all_frames, spatial_tolerance, temporal_tolerance):
+def smooth_objects(all_frames):
+  tracker_list = [] 
   # Since we assume that objects cannot change types, we separately run the
   # object tracking algorithm for each object type
   unique_obj_types = set([obj['name'] for frame in all_frames for obj in frame])
-  print(unique_obj_types)
+  print('Unique object types: ' + str(unique_obj_types))
 
-  for obj_type in unique_obj_types:
-    for frame in all_frames:
-      print(obj_type + ': ' + str([calc_centroid(obj) for obj in frame if obj['name'] is obj_type]))
-    # all_frames_centroids_by_type = [[calc_centroid(obj) for obj in frame if obj['name'] is obj_type] for frame in all_frames]
+  # Initialize a centroid tracker for each object type
+  trackers = {obj_type : CentroidTracker(maxDisappeared = 15) for obj_type in unique_obj_types}
 
-with open('video_frame_analysis_YOLOv3.pickle', 'rb') as in_file:
+  for (frame_idx, frame) in enumerate(all_frames):
+
+    new_frame_list = []
+    for obj_type in unique_obj_types:
+      trackers[obj_type].update([obj['box_points'] for obj in frame if obj['name'] is obj_type]) # Update the centroid tracker
+
+      for (ID, centroid) in trackers[obj_type].objects.items():
+        new_ID = obj_type + '_' + str(ID) # Concatenate object type with object ID
+        for obj in frame:
+          if obj['name'] is obj_type:
+          # Since we need to output (ID, bounding box) and Centroid Tracker doesn't record bounding boxes
+          # match each ID with its bounding box by centroid; this solution implicitly assumes each object of a
+          # specified type within each frame has a distinct centroid
+            obj_centroid = calc_centroid(obj['box_points'])
+            if (abs(obj_centroid[0] - centroid[0]) < sys.float_info.epsilon) and (abs(obj_centroid[1] - centroid[1]) < sys.float_info.epsilon):
+              new_frame_list.append((new_ID, obj['box_points']))
+
+    tracker_list.append(new_frame_list)
+
+    # TODO: Replace numerical obj ID with obj_type + '_' + str(ID) string
+  # for (frame_idx, objects) in enumerate(tracker_list):
+  #   print('Frame ' + str(frame_idx) + ': ' + str(objects))
+  return tracker_list
+
+def compute_durations(tracker_list):
+  object_durations = {}
+  for (frame_idx, frame_list) in enumerate(tracker_list):
+    for obj in frame_list:
+      if obj[0] in object_durations:
+        object_durations[obj[0]] = (object_durations[obj[0]][0], frame_idx)
+      else:
+        object_durations[obj[0]] = (frame_idx, frame_idx)
+  return object_durations
+
+# This function removes objects from tracker_list if they exist for a shorter time than min_duration frames
+def remove_brief_objects(tracker_list, object_durations, min_duration = 15):
+  return [[obj for obj in frame_list if
+            (object_durations[obj[0]][1] - object_durations[obj[0]][0]) >= min_duration]
+            for frame_list in tracker_list]
+
+def sample_targets(tracker_list, object_durations, min_duration = 30, max_duration = 75):
+  target_list = []
+  next_switch_frame = -1
+  for (frame_idx, frame_objects) in enumerate(tracker_list):
+
+    if frame_idx > next_switch_frame:
+      # weights = np.array([1 for obj in tracker_list[frame_idx]])
+      weights = np.array([object_durations[obj[0]][1] - frame_idx for obj in tracker_list[frame_idx]])
+      weights = weights / weights.sum()
+      current_target_idx = np.random.choice(range(len(weights)), p = weights)
+      current_target = tracker_list[frame_idx][current_target_idx][0]
+      print(current_target)
+      next_switch_frame = object_durations[current_target][1] # TODO: Potentially shorten this time
+
+    found_target = False
+    for obj in tracker_list[frame_idx]:
+      print(obj[0], current_target)
+      if obj[0] == current_target:
+        found_target = True
+        target_list.append(obj)
+    if not found_target:
+      target_list.append(target_list[-1])
+
+  return target_list
+
+def smooth_and_display_objects():
+
+  tracker_list = smooth_objects(all_frames)
+  object_durations = compute_durations(tracker_list)
+  target_list = sample_targets(tracker_list, object_durations)
+
+  video = cv2.VideoCapture('data/MOT17_videos/13.mp4')
+
+  # Get basic video information
+  video_width = video.get(cv2.CAP_PROP_FRAME_WIDTH)
+  video_height = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
+  FPS = video.get(cv2.CAP_PROP_FPS) # natural frame rate
+  print('Video framerate:' + str(FPS))
+
+  # Set the inter-frame delay based on the video's natural framerate
+  delay = 1.0/FPS
+  
+  # Create video display window and center it on the screen
+  screen = screeninfo.get_monitors()[0]
+  # (xPos, yPos) is the upper-left corner of the video display
+  xPos = int((screen.width - video_width) / 2.0)
+  yPos = int((screen.height - video_height) / 2.0)
+  if xPos < 0 or yPos < 0:
+    print('Warning: video size ' + str((video_width, video_height)) + \
+          ' is larger than screen size ' + str((screen.width, screen.height)) + '.')
+    print('Some of the video may be cut off.')
+  cv2.namedWindow('Video Frame')
+  cv2.moveWindow('Video Frame', xPos, yPos)
+  
+  # Set up to display the first frame
+  current_frame = 0
+  videoStartTime = time.time()
+  videoIsPlaying = True
+  nextFrameExists, frame = video.read() # Load first video frame
+  
+  print("Playing video...")
+  while nextFrameExists: # While there are more frames to display, continue displaying video
+    if time.time() > videoStartTime + current_frame * delay:
+
+        cv2.imshow('Video Frame', frame) # Display current frame
+        cv2.waitKey(1)
+        nextFrameExists, frame = video.read() # Load next video frame
+
+        # Draw ellipse around and label target object
+        object_ID, b = target_list[current_frame]
+        centroid = calc_centroid(b)
+        cv2.putText(frame, object_ID, (centroid[0] - 10, centroid[1] - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+        cv2.ellipse(frame, centroid, ((b[2] - b[0])//2, (b[3] - b[1])//2), 0, 0, 360, color = (0, 255, 0), thickness = 2)
+
+        current_frame += 1
+
+  video.release()
+  cv2.destroyAllWindows()
+
+with open('data/detected_objects/13.pickle', 'rb') as in_file:
   all_frames = pickle.load(in_file)
 
-smooth_objects(all_frames,2,3)
+smooth_and_display_objects()
