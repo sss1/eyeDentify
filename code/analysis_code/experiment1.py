0b06540132ac16c98b7a206b2fab6e6a9460f421
Shashank Singh <shashanksi@google.com>
2020-02-07 20:52:24 -0500

    Started sketching out Experiment 1 preprocessing.

--- code/analysis_code/experiment1.py
+++ code/analysis_code/experiment1.py
@@ -1,19 +1,31 @@
 # This module performs the analyses for Experiment 1: Guided Viewing with Detected Targets
 # import numpy as np
-import pickle
+import pickle, sys
 
 import load_and_preprocess_data
 
+sys.path.insert(1, '../util')
+import centroidtracker
+
 # Load participant data
 num_participants = 1
-# participants = [load_and_preprocess_data.load_participant(i) for i in range(num_participants)]
-# participants = load_participants(num_participants)
+participants = [load_and_preprocess_data.load_participant(i) for i in range(num_participants)]
 
 # Load object detection data
+# TODO: Apply centroid tracker to detected objects using object_smoothing._smooth_objects()
 detected_objects = []
 for video_idx in range(1, 15):
   with open('../../data/detected_objects/' + str(video_idx).zfill(2) + '.pickle', 'rb') as in_file:
     detected_objects.append(pickle.load(in_file))
+
+for participant in participants:
+  for (experiment_video_data, video_objects) in zip(participant.frames_by_video, detected_objects):
+    # TODO: Initialize HMM here
+    for (experiment_frame_data, detected_objects_in_frame) in zip(experiment_video_data, video_objects):
+      print(experiment_frame_data.gaze, experiment_frame_data.target.centroid)
+      print([centroidtracker.calc_centroid(obj['box_points']) for obj in detected_objects_in_frame])
+      print()
+      # TODO: Update HMM here
 
 # for participant in participants:
   # TODO:
