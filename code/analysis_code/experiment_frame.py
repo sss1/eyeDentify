e77f5a79fa07a4c6a36d9e61e7b3a89b97eed85d
sss1 <sss1@andrew.cmu.edu>
2020-02-09 21:54:42 -0500

    Finalized class structure (object_frame -> experiment_frame -> experiment_video -> participant) for data analysis.

--- code/analysis_code/experiment_frame.py
+++ code/analysis_code/experiment_frame.py
@@ -1,21 +1,27 @@
-"""This module specifies the frame class."""
+"""This module specifies the ExperimentFrame class."""
 
 import object_frame
 
 class ExperimentFrame:
   """Eye-tracking and target information for a single frame."""
-  def __init__(self, target: object_frame.ObjectFrame):
+  def __init__(self, video_idx: int, t: float, video_frame: int,
+               target: object_frame.ObjectFrame):
     """Initialize with target information; eyetrack is added later.
     
     Args:
+      video_idx: Index (between 1-14, inclusive) of the video being displayed.
+      t: Time (in ms) since the epoch, according to the experiment computer.
+      video_frame: Index of the frame of the video being displayed
       target: Target object during that frame.
     """
+    self.video_idx = video_idx
+    self.t = t
     self.target = target
-    self.t = target.t
-    self.video_idx = target.video_idx
-    self.video_frame = target.video_frame
+    self.video_frame = video_frame
 
   def set_eyetrack(self, gazeX, gazeY, diam):
     self.gaze = (gazeX, gazeY)
     self.diam = diam
 
+  def set_detected_objects(self, detected_objects):
+    self.detected_objects = detected_objects
--- code/analysis_code/object_frame.py
+++ code/analysis_code/object_frame.py
@@ -2,31 +2,32 @@
 
 import scipy.stats
 
+from typing import Tuple
+
 class ObjectFrame:
   """Information about a single object in a single frame."""
-  def __init__(self, t: float, video_idx: int, video_frame: int,
-          object_name: str, x: float, y: float, x_radius: float,
-          y_radius: float):
+  def __init__(self, class_name: str, object_index: int,
+               centroid: Tuple[float, float],
+               size: Tuple[float, float]):
     """Initialize an ObjectFrame object.
 
     Args:
-      t: Time (in ms) since the epoch, according to the experiment computer.
-      video_idx: Index (between 1-14, inclusive) of the video being displayed
-      video_frame: Index of the frame of the video being displayed
-      target_name: Name (COCO class) of the target object
-      x: x-coordinate of center of object bounding box
-      y: y-coordinate of center of object bounding box
-      x_radius: half-width of the target
-      y_radius: half-height of the target
+      class_name: COCO class of the object
+      object_index: Index (in order of appearance) of that object within its
+        object class and video (e.g., chair 5 is the 5th chair appearing in the
+        video). Note that the (class_name, object_index) pair uniquely
+        identifies a object (which may last multiple frames) within a video
+      centroid: (x, y)-coordinates of center of object bounding box
+      size: (half-width, half-height) of object bounding box
     """
-    self.t = t
-    self.video_idx = video_idx
-    self.video_frame = video_frame
+    self.class_name = class_name
+    self.object_index = object_index
+    self.centroid = centroid
+    self.size = size
 
-    self.class_name = object_name.split('_')[0]
-    self.object_idx = int(object_name.split('_')[1])
-    self.centroid = (x, y)
-    self.size = (x_radius, y_radius)
+  def is_same_object(self, other):
+    return (self.class_name == other.class_name
+            and self.object_index == other.object_index)
 
   def emission_density(self, point):
     """Returns the value of the object's emission density at a point."""
