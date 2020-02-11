6b893537ae61f141824312bb893cf14c4da7387d
Shashank Singh <shashanksi@google.com>
2020-02-11 17:39:47 -0500

    Implemented (but have not polished or tested) HMM forwards update.

--- code/analysis_code/hmm.py
+++ code/analysis_code/hmm.py
@@ -2,11 +2,15 @@
 """
 
 import numpy
-
-from typing import Dict, List
+import math
+from typing import Dict, List, NewType, Tuple
 
 import experiment_frame
 import object_frame
+
+FrameTable = NewType('FrameTable',
+                     Dict[object_frame.ObjectFrame,
+                          Tuple[float, object_frame.ObjectFrame]])
 
 class HMM:
   """An hidden Markov model of a single data sequence.
@@ -14,12 +18,12 @@ class HMM:
   Example usage:
     hmm = HMM(sigma, tau)
     for experiment_frame, objects_in_frame in zip(experiment_frames, detected_objects):
-      hmm.update(experiment_frame, objects_in_frame)
+      hmm.forwards_update(experiment_frame.gaze, objects_in_frame)
     mle = hmm.backwards()
 
   Hidden Attributes:
-    likelihood_table: mapping from each object to its partial maximum likelihood
-      and most likely predecessor
+    likelihood_table: (List[FrameTable]) for each frame, a dict mapping each
+      object to its partial maximum log-likelihood and most likely predecessor
   """
 
   def __init__(self, sigma: float, tau: float):
@@ -31,19 +35,60 @@ class HMM:
     """
     self.sigma = sigma
     self.tau = tau
-    self.likelihood_table : Dict[object_frame.ObjectFrame,
-                                 (float, object_frame.ObjectFrame)] = {}
+    self.likelihood_table = []
 
-  def forwards_update(experiment_frame: experiment_frame.ExperimentFrame,
+  def forwards_update(self, gaze: Tuple[float, float],
                       objects_in_frame: List[object_frame.ObjectFrame]):
     """Performs an update step of the forwards algorithm based on input data.
+
     Args:
       experiment_frame: a single frame of participant data
       objects_in_frame: list of objects detected in frame
     """
-    raise NotImplemented()
 
-  def backwards() -> List[object_frame.ObjectFrame]:
+    if not self.likelihood_table:
+      # This is the first frame; only use emission probabilities
+      new_frame_table = {obj : (obj.log_emission_density(gaze, self.sigma), None)
+                         for obj in objects_in_frame}
+    else:
+      new_frame_table = _compute_next_frame_table(self.likelihood_table[-1],
+                                                  gaze, objects_in_frame)
+    self.likelihood_table.append(new_frame_table)
+
+
+  def _compute_next_frame_table(prev_frame_table: FrameTable,
+                                gaze: Tuple[float, float],
+                                objects_in_frame: List[object_frame.ObjectFrame]):
+    """Computes a frame_table using a previous frame table.
+
+    NOTE: Depending on sigma and tau, this implementation may bias transitions
+    to frames where the tracked object disappears.
+    """
+    num_new_objects = len(objects_in_frame)
+    new_frame_table = {obj : (float('-inf'), None) for obj in objects_in_frame}
+
+    for prev_obj, (prev_obj_partial_log_likelihood, _) in prev_frame_table:
+
+      prev_obj_in_new_frame = (prev_obj in objects_in_frame)
+
+      for new_obj in objects_in_frame:
+
+        if prev_obj_in_new_frame and prev_obj == new_obj:
+          transition_probability = self.tau
+        elif prev_obj_in_new_frame:
+          transition_probability = (1 - self.tau)/(num_new_objects - 1)
+        else:
+          transition_probability = 1/num_new_objects
+
+        new_partial_log_likelihood = (
+            prev_obj_partial_log_likelihood
+            + math.log(transition_probability)
+            + new_obj.log_emission_density(gaze, self.sigma))
+        if new_partial_log_likelihood > new_frame_table[new_obj][0]:
+          new_frame_table[new_obj] = (new_partial_log_likelihood, prev_obj)
+    return new_frame_table
+
+  def backwards(self) -> List[object_frame.ObjectFrame]:
     """Runs the backwards algorithm to compute the object sequence MLE.
 
     Returns:
