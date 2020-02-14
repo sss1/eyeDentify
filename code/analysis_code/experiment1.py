62b1bb4776eca0408317ab0dfdd561478c47845b
Shashank Singh <shashanksi@google.com>
2020-02-14 11:27:10 -0500

    Style improvements and updated TODOs.

--- code/analysis_code/experiment1.py
+++ code/analysis_code/experiment1.py
@@ -1,42 +1,39 @@
 # This module performs the analyses for Experiment 1: Guided Viewing with Detected Targets
-# import numpy as np
 import pickle, sys
 
+import hmm
 import load_and_preprocess_data
 import util
-import centroidtracker
 
-from typing import List
-
-import participant
+SIGMA = 1
+TAU = 0.9
 
 VIDEOS = range(1, 15)
 NUM_PARTICIPANTS = 1
 
+DETECTION_DATA_DIR = '../../data/detected_objects'
+
 # Load participant data
 participants = [load_and_preprocess_data.load_participant(i) for i in range(NUM_PARTICIPANTS)]
-
 print('Loaded data from {} participants.'.format(NUM_PARTICIPANTS))
 
 # Load object detection data
 detected_objects = []
 for video_idx in VIDEOS:
-  with open('../../data/detected_objects/' + str(video_idx).zfill(2) + '.pickle', 'rb') as in_file:
+  detection_data_fname = '{}/{}.pickle'.format(DETECTION_DATA_DIR,
+                                               str(video_idx).zfill(2))
+  print('Loading object detection data from {}...'.format(detection_data_fname))
+  with open(detection_data_fname, 'rb') as in_file:
     all_frames = pickle.load(in_file)
   detected_objects.append(util.smooth_objects(all_frames))
 
 for participant in participants:
   for (experiment_video_data, video_objects) in zip(participant.videos, detected_objects):
-    # TODO: Initialize HMM here
-    for (experiment_frame_data, detected_objects_in_frame) in zip(experiment_video_data.frames, video_objects):
-      print(experiment_frame_data.gaze, experiment_frame_data.target.centroid, experiment_frame_data.target.class_name)
-      print([obj.centroid for obj in detected_objects_in_frame])
-      print()
-      # TODO: Update HMM here
-
-# for participant in participants:
-  # TODO:
-  # 1) Load object detection data
-  # 2) Run HMM
-  # 3) Omit data for 300 ms after each target switch
-  # 4) Compute agreement between target and HMM
+    print('Initializing HMM...')
+    trial_hmm = hmm.HMM(SIGMA, TAU)
+    print('Running HMM forwards pass...')
+    for (frame_idx, (experiment_frame_data, detected_objects_in_frame)) in enumerate(zip(experiment_video_data.frames, video_objects)):
+      trial_hmm.forwards_update(experiment_frame_data.gaze,
+                          detected_objects_in_frame)
+    print('Running HMM backwards pass...')
+    # TODO: run trial_hmm.backwards() to retrieve MLE, and compare with target
